5d37bb3d6729f50084c21db07a6d6b60d14ce8a9
ahmedHamed012 <ahmedcina60@yahoo.com>
2025-05-23 17:28:36 +0300

    feat: add AllContacts and CreateContact components with HTML, SCSS, and TypeScript files

---
+++ src/app/components/Contacts/all-contacts/all-contacts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-contacts',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-contacts.component.html',
+  styleUrl: './all-contacts.component.scss'
+})
+export class AllContactsComponent {
+
+}
