c30906516fa69aa376e2a7a47e2243a7e437b1a0
ahmedHamed012 <ahmedcina60@yahoo.com>
2025-05-23 16:06:57 +0300

    feat: enhance app configuration and routing
    - Updated app.config.ts to include PrimeNG and animations providers.
    - Modified app.routes.ts to add lazy loading for authentication routes.
    - Created environment files for development and production.
    - Added global styles in styles.scss, including a new font and color variables.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Route, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./components/Authentication/auth.routes').then(
+        (m) => m.AuthRoutes
+      ),
+  },
+];
